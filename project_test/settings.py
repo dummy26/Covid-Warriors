80a3b31396cb6e4da93a0b34c845977ae2803fe4
Ani <dummyani26@gmail.com>
2020-05-30 17:24:29 +0530

    removed crispy forms from INSTALLED_APPS

--- project_test/settings.py
+++ project_test/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
