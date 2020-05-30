24553dc65e87a995612fdd3d28f921b8e920c2e4
Ani <dummyani26@gmail.com>
2020-05-30 17:39:57 +0530

    ALLOWED_HOSTS and Procfile

--- project_test/settings.py
+++ project_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's6jcke(03fi$13$s(r29u4)ti8h-p$w$%wr0tkihwg#2@v!7i+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coronawarriors.herokuapp.com']
 
 
 # Application definition
