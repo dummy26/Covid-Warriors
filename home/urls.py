f1658ed9ac81e2cb4645b63dfcf31adb914ed77a
Ani <dummyani26@gmail.com>
2020-05-31 06:42:03 +0530

    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from .import views
 from .views import ArticleListView, ArticleDetailView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='homepage'),
@@ -9,6 +11,6 @@ urlpatterns = [
     path('live_tracker', views.live_tracker, name='live_tracker'),
     path('search', views.search, name='search'),
     # path('search_tab', views.search_tab, name='search_tab')
-
-
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
