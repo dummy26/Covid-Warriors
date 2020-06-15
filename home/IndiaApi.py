6ef83eddc8257697c658c2f29a2527f9643ec5df
Ani <dummyani26@gmail.com>
2020-06-15 07:25:14 +0530

    design changes in india_tracker, search_result, search_tab and removed unecessary code from admin, models and IndiaApi

--- home/IndiaApi.py
+++ home/IndiaApi.py
@@ -1,12 +1,8 @@
 import json
-import geopandas as gpd
 from bs4 import BeautifulSoup
 import requests
-import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib
-# To solve RuntimeError
-matplotlib.use('Agg')
 
 url = 'https://www.mohfw.gov.in/'
 web_content = requests.get(url).content
@@ -93,33 +89,3 @@ def save_data():
     data_json = state_data.to_json()
     with open('home/data.txt', 'w') as f:
         f.write(data_json)
-
-
-def save_heat_map():
-    map_data = gpd.read_file('./media/india_map/Indian_States.shp')
-    map_data.rename(columns={'st_nm': 'States/UT'}, inplace=True)
-
-    # correct the name of states in the map dataframe
-    map_data['States/UT'] = map_data['States/UT'].str.replace('&', 'and')
-    map_data['States/UT'].replace('Arunanchal Pradesh',
-                                  'Arunachal Pradesh', inplace=True)
-    map_data['States/UT'].replace('Dadara and Nagar Havelli',
-                                  'Dadar Nagar Haveli', inplace=True)
-    map_data['States/UT'].replace('Andaman and Nicobar Island',
-                                  'Andaman and Nicobar Islands', inplace=True)
-    map_data['States/UT'].replace('NCT of Delhi', 'Delhi', inplace=True)
-    map_data['States/UT'].replace('Telangana', 'Telengana', inplace=True)
-
-    # merge state_data and map_data
-    merged_data = pd.merge(map_data, state_data, how='left', on='States/UT')
-    merged_data.fillna(0, inplace=True)
-    merged_data.drop('Sr.No', axis=1, inplace=True)
-
-    # create figure and axes for Matplotlib
-    fig, ax = plt.subplots(1, figsize=(20, 12))
-    ax.axis('off')
-    ax.set_title('Covid-19 Statewise Data')
-    merged_data.plot(column='Confirmed', cmap='tab10',
-                     linewidth=0.8, ax=ax, edgecolor='0.6', legend=True)
-    plt.gcf().set_size_inches(7, 6)
-    plt.savefig('./media/figures/heat_map.jpg')
--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-from .models import Article
-
-# Remove when homepage and article tab is ready 
-admin.site.register(Article)
